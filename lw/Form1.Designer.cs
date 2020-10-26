b21727d61406c8b2dd73fda2a085c2f7fd513c6b
KazzModan <kklopov64@gmail.com>
2020-10-26 23:45:45 +0200

    Added HM

---
+++ lw/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace lw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(20, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Bykovinska",
+            "Kievska",
+            "Kurchatova"});
+            this.comboBox2.Location = new System.Drawing.Point(165, 62);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(292, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(22, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox3.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Bitch",
+            "Cleaner",
+            "Manager",
+            "Admin"});
+            this.comboBox3.Location = new System.Drawing.Point(320, 63);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(447, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(20, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 165);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(320, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(507, 193);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(311, 48);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Add";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "City";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Street";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(317, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Position";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(317, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Salary";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Name";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 378);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(803, 23);
+            this.progressBar1.TabIndex = 15;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(507, 64);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(311, 123);
+            this.checkedListBox1.TabIndex = 16;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(507, 247);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(311, 49);
+            this.btnDel.TabIndex = 17;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(507, 302);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(311, 57);
+            this.btnEdit.TabIndex = 18;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(830, 450);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnEdit;
+    }
+}
+
---
+++ lw/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace lw
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1_TextUpdate(null, null);
+        }
+
+        private void comboBox1_TextUpdate(object sender, EventArgs e)
+        {
+            int counter = 0;
+            foreach (var item in comboBox1.Items)
+            {
+                if (comboBox1.Text == item.ToString())
+                    counter++;
+            }
+            if (counter == 0)
+                comboBox1.Items.Add(comboBox1.Text);
+            else
+                MessageBox.Show("This city already exist");
+        }
+        private void comboBox2_TextUpdate(object sender, EventArgs e)
+        {
+            int counter = 0;
+            foreach (var item in comboBox2.Items)
+            {
+                if (comboBox2.Text == item.ToString())
+                    counter++;
+            }
+            if (counter == 0)
+                comboBox2.Items.Add(comboBox2.Text);
+            else
+                MessageBox.Show("This street already exist");
+        }
+        private void comboBox3_TextUpdate(object sender, EventArgs e)
+        {
+            int counter = 0;
+            foreach (var item in comboBox3.Items)
+            {
+                if (comboBox3.Text == item.ToString())
+                    counter++;
+            }
+            if (counter == 0)
+                comboBox3.Items.Add(comboBox3.Text);
+            else
+                MessageBox.Show("This position already exist");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox2_TextUpdate(null, null);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadFromFile();
+            comboBox1.Items.AddRange(LoadParameter("..\\..\\City.txt"));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            comboBox3_TextUpdate(null, null);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text + " " + comboBox1.Text + " " + comboBox2.Text
+                + " " + comboBox3.Text + " " + textBox2.Text + "$";
+            if (comboBox1.Text != "" && comboBox2.Text != ""
+                && comboBox3.Text != "" && textBox1.Text != "" && textBox2.Text != "")
+                checkedListBox1.Items.Add(text);
+            else
+                MessageBox.Show("one of fields is empty");
+            WriteToFile();
+        }
+        void WriteToFile()
+        {
+            using (StreamWriter writer = new StreamWriter("..\\..\\text.txt"))
+            {
+
+                foreach (var item in checkedListBox1.Items)
+                {
+                    writer.WriteLine(item.ToString());
+                }
+            }
+        }
+        void ReadFromFile()
+        {
+            using (StreamReader reader = new StreamReader("..\\..\\text.txt"))
+            {
+                string str = "";
+                while (!reader.EndOfStream)
+                {
+                    str = reader.ReadLine();
+                    checkedListBox1.Items.Add(str);
+                }
+                LaunchProgressBar();
+            }
+        }
+        void LaunchProgressBar()
+        {
+            progressBar1.Maximum = checkedListBox1.Items.Count;
+            progressBar1.Step = 1;
+            for (int i = 0; i < progressBar1.Maximum; i++)
+            {
+                progressBar1.PerformStep();
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.CheckedItems.Count != 0)
+            {
+                int size = checkedListBox1.CheckedItems.Count;
+                for (int i = 0; i < size; i++)
+                    checkedListBox1.Items.Remove(checkedListBox1.CheckedItems[0]);
+            }
+            else
+                MessageBox.Show("Please, choose at least 1 item");
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "" || comboBox2.Text == ""
+             || comboBox3.Text == "" || textBox1.Text == "" || textBox2.Text == "")
+                MessageBox.Show("one of fields is empty");
+            else if (checkedListBox1.CheckedItems.Count == 1)
+            {
+                string text = textBox1.Text + " " + comboBox1.Text + " " + comboBox2.Text
+              + " " + comboBox3.Text + " " + textBox2.Text + "$";
+                int index = checkedListBox1.Items.IndexOf(checkedListBox1.SelectedItem);
+                List<string> list = new List<string>();
+                for (int i = index + 1; i < checkedListBox1.Items.Count; i++)
+                {
+                    list.Add(checkedListBox1.Items[i].ToString());
+                    checkedListBox1.Items.Remove(checkedListBox1.Items[i]);
+                }
+                checkedListBox1.Items.RemoveAt(index);
+                checkedListBox1.Items.Add(text);
+                checkedListBox1.Items.AddRange(list.ToArray());
+            }
+            else if (checkedListBox1.CheckedItems.Count == 0)
+                MessageBox.Show("Please, choose at least 1 item");
+
+            else
+                MessageBox.Show("You choosed too many items");
+        }
+
+        private string[] LoadParameter(string file)
+        {
+            if (!File.Exists(file))
+                return new string[0];
+
+            string[] splitDate = new string[0];
+
+
+            try
+            {
+                string data = File.ReadAllText(file);
+                splitDate = data.Split('\n');
+
+                string[] delete = new string[]
+                {
+                        "<option>",
+                        "</option>",
+                };
+
+                for (int i = 0; i < splitDate.Length; i++)
+                {
+                    for (int j = 0; j < delete.Length; j++)
+                    {
+                        int index = splitDate[i].IndexOf(delete[j]);
+
+                        if (index == -1)
+                            continue;
+
+                        splitDate[i] = splitDate[i].Remove(index, delete[j].Length);
+                     
+                    }
+                    int indexR = splitDate[i].Length - 1;
+
+                    if (indexR == -1)
+                        continue;
+
+                    splitDate[i] = splitDate[i].Remove(indexR, 1);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("corrapted file");
+                return new string[0];
+            }
+
+            return splitDate;
+        }
+    }
+}
